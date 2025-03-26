881c93b1ff73bd5fb21dae0d1e1096100ea85a26
Gayatri132005 <gayatrikotwal13@gmail.com>
2025-03-26 19:30:31 +0530

    initial

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Allow access from outside
-    port: process.env.PORT || 4000, // Use Render's port or fallback to 5173
-  },
-});
+})
